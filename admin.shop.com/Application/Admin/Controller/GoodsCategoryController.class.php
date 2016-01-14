2b0d7a84ca8e87970c4b3419ce6ebdd1cbf3d0ac
jiege <jiege@qq.com>
2016-01-14 11:30:31 +0800

    正在操作商品分类的增加功能

--- admin.shop.com/Application/Admin/Controller/GoodsCategoryController.class.php
+++ admin.shop.com/Application/Admin/Controller/GoodsCategoryController.class.php
@@ -17,4 +17,26 @@ class GoodsCategoryController extends BaseController
               $this->assign('meta_title', $this->meta_title);
               $this->display('index');
        }
+
+
+       public function add()
+       {
+              if (IS_POST) {
+
+                     //>>2. 开启自动验证
+                     if ($this->model->create() !== false) {
+//                dump(I('post.'));
+//                exit;
+                            if ($this->model->add() !== false) {
+                                   $this->success('添加成功!', U('index'));
+                                   return;
+                            }
+                     }
+                     $this->error('验证失败' . show_model_error($this->model));
+              } else {
+                     //展示添加页面
+                     $this->assign('meta_title', '添加'.$this->meta_title);
+                     $this->display('edit');
+              }
+       }
 }
